5136c2da45864c70e29b2d003037b98fc6564987
samuel <adobayayawosamuel63@gmail.com>
2026-02-11 14:38:25 +0100

    c'est la fin de l'exercice 2

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ for(let i = 0 ; i < liste.length;i++){
 console.log("Indices pairs: ", listePaire) 
 console.log("Indices impaires: ", listeInpaire)}
 // console.log(filtre(liste))
+
+// Exercice 2 : Comparaison de voitures (3pts)
+// Énoncé :
+// On vous donne les objets suivants représentant des voitures :
+// const voiture1 = {
+// marque: "Renault",
+// modele: "Clio",
+// prix: 13000
+// };
+// const voiture2 = {
+// marque: "Renault",
+// modele: "Megane",
+// prix: 18000
+// };
+// Consignes :
+// 1. Créez un programme qui affiche la marque et le modèle 
+// de la voiture la plus
+// chère.
+// Exemple de résultat attendu :
+// Voiture la plus chère : Renault Megane
+
+const voiture1 = {
+marque: "Renault",
+modele: "Clio",
+prix: 13000
+};
+const voiture2 = {
+marque: "Renault",
+modele: "Megane",
+prix: 18000
+};
+
+if( voiture1.prix > voiture2.prix){
+    console.log("Voiture la plus chère :" , voiture1.marque , voiture1.modele)
+} else{
+        console.log("Voiture la plus chère :" , voiture2.marque , voiture2.modele)
+
+}
+
+// fin de l'exercice 2
